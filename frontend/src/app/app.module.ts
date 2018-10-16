a88b85fc852289ac9efcc925dc725d747d65d8e5
Grob Oliver <groboliver1117@gmail.com>
2018-10-16 11:09:42 +0200

    Components skeleton created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,29 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component';
-import {NavbarComponent} from './components/navbar/navbar.component';
-import {LoginComponent} from './components/login/login.component';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { UserPageComponent } from './components/user-page/user-page.component';
+import { FavouritesComponent } from './components/user-page/favourites/favourites.component';
+import { WatchlistComponent } from './components/user-page/watchlist/watchlist.component';
+import { WatchedComponent } from './components/user-page/watched/watched.component';
+import { SearchComponent } from './components/search/search.component';
+import { SeriesDetailComponent } from './components/series-detail/series-detail.component';
+import { TrendingComponent } from './components/trending/trending.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
+    UserPageComponent,
+    FavouritesComponent,
+    WatchlistComponent,
+    WatchedComponent,
+    SearchComponent,
+    SeriesDetailComponent,
+    TrendingComponent
   ],
   imports: [
     BrowserModule
