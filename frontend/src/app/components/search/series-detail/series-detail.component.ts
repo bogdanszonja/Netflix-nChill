cb9c58d575c03eb78f3318a1818b0db9355ee5ba
bogdanszonja <bogdanszonja@gmail.com>
2018-10-17 14:34:18 +0200

    it is possible for the user to add series, seasons and episodes

--- frontend/src/app/components/search/series-detail/series-detail.component.ts
+++ frontend/src/app/components/search/series-detail/series-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Series } from "../../../models/Series";
+import {Episode} from "../../../models/Episode";
+import {Season} from "../../../models/Season";
+import {UserService} from "../../../services/user/user.service";
+import {Observable} from "rxjs/internal/Observable";
 
 @Component({
   selector: 'app-series-detail',
@@ -10,9 +14,24 @@ export class SeriesDetailComponent implements OnInit {
 
   @Input() series: Series;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+  }
+
+  addWholeSeries(series: Series):void {
+    console.log("all seasons added");
+    this.userService.addWholeSeries(series);
+  }
+
+  addSingleSeason(season: Season):void {
+    console.log("season added");
+    this.userService.addSingleSeason(season);
+  }
+
+  addSingleEpisode(episode: Episode):void {
+    console.log("episode added");
+    this.userService.addSingleEpisode(episode);
   }
 
 }
