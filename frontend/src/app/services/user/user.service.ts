3426abb49adf2494a3bad5e6cd8f72b35723577d
bogdanszonja <bogdanszonja@gmail.com>
2018-10-17 14:35:50 +0200

    service to the server to update user attributes

--- frontend/src/app/services/user/user.service.ts
+++ frontend/src/app/services/user/user.service.ts
@@ -1,19 +1,65 @@
 import { Injectable } from '@angular/core';
 
 import { Subject } from 'rxjs/internal/Subject';
+import {Series} from "../../models/Series";
+import {Observable} from "rxjs/internal/Observable";
+import {Season} from "../../models/Season";
+import {Episode} from "../../models/Episode";
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {catchError, tap} from "rxjs/operators";
+import {of} from "rxjs/index";
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  private url = 'http://localhost:8080/user';
+
   loginStatus = new Subject<string>();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   handleLogin(type: string) {
     console.log(this.loginStatus);
     this.loginStatus.next(type);
+  }
+
+  addWholeSeries(series: Series):Observable<Series> {
+    return this.http.post<Series>(this.url, series, httpOptions)
+      .pipe(
+        tap(_ => console.log(`Series added`)),
+        catchError(this.handleError<Series>())
+      );
+  }
+
+  addSingleSeason(season: Season):Observable<Season> {
+    return this.http.post<Season>(this.url, season, httpOptions)
+      .pipe(
+        tap(_ => console.log(`Series added`)),
+        catchError(this.handleError<Season>())
+      );
+  }
+
+  addSingleEpisode(episode: Episode):Observable<Episode> {
+    return this.http.post<Episode>(this.url, episode, httpOptions)
+      .pipe(
+        tap(_ => console.log(`Series added`)),
+        catchError(this.handleError<Episode>())
+      );
+  }
+
+  private handleError<T> (result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    };
   }
 
 }
