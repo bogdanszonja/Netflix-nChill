0db582221123e67ca06171fbb1943f1621078ff6
bogdanszonja <bogdanszonja@gmail.com>
2018-10-18 15:16:09 +0200

    user model created

---
+++ frontend/src/app/models/User.ts
@@ -0,0 +1,15 @@
+import {Series} from './Series';
+import {Episode} from "./Episode";
+
+export class User {
+
+  id: number;
+  username: string;
+  emailAddress: string;
+  password: string;
+  watchList: Series[];
+  favourites: Series[];
+  watchedEpisodes: Episode[];
+  registrationDate: Date;
+
+}
