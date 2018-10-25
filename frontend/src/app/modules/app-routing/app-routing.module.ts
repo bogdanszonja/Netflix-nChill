34e0e5b8c7366bab9f2b9618a29983df8ee0a600
Grob Oliver <groboliver1117@gmail.com>
2018-10-25 14:52:38 +0200

    New outlet created for login

--- frontend/src/app/modules/app-routing/app-routing.module.ts
+++ frontend/src/app/modules/app-routing/app-routing.module.ts
@@ -4,19 +4,29 @@ import { RouterModule, Routes } from '@angular/router';
 import { SearchComponent } from '../../components/search/search.component';
 import { UserPageComponent } from '../../components/user-page/user-page.component';
 import { TrendingComponent } from '../../components/trending/trending.component';
+import { LoginComponent } from '../../components/login/login.component';
+import { WatchlistComponent } from '../../components/user-page/watchlist/watchlist.component';
+import { FavouritesComponent } from '../../components/user-page/favourites/favourites.component';
+import { WatchedComponent } from '../../components/user-page/watched/watched.component';
 
 const routes: Routes = [
   {path: '', component: TrendingComponent},
-  {path: 'login', component: TrendingComponent},
+  {path: 'login', component: LoginComponent, outlet: 'login'},
   {path: 'search', component: SearchComponent},
-  {path: 'user-page', redirectTo: 'user-page/watchlist', pathMatch: 'full'},
-  {path: 'user-page/watchlist', component: UserPageComponent},
-  {path: 'user-page/favourites', component: UserPageComponent},
-  {path: 'user-page/watched', component: UserPageComponent}
+  {
+    path: 'user-page',
+    component: UserPageComponent,
+    children: [
+      {path: 'watchlist', component: WatchlistComponent},
+      {path: 'favourites', component: FavouritesComponent},
+      {path: 'watched', component: WatchedComponent},
+      {path: '', redirectTo: 'watchlist', pathMatch: 'full'}
+    ]
+  }
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [ RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'}) ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
