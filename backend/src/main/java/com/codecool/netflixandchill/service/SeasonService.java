df969171c661ef86a69539922d404e2c479bd237
ZoltanSari <Kingshapy@gmail.com>
2018-11-13 10:49:29 +0100

    fixed add series, season and episode to watchedList

--- backend/src/main/java/com/codecool/netflixandchill/service/SeasonService.java
+++ backend/src/main/java/com/codecool/netflixandchill/service/SeasonService.java
@@ -2,14 +2,11 @@ package com.codecool.netflixandchill.service;
 
 import com.codecool.netflixandchill.model.Season;
 import com.codecool.netflixandchill.repository.SeasonRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SeasonService {
-    private static Logger logger = LoggerFactory.getLogger(SeasonService.class);
 
     @Autowired
     private SeasonRepository seasonRepository;
